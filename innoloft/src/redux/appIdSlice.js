c7d792ffb7057269ed2de4b072294930896b2a4c
matkot11 <matkot11@gmail.com>
2021-03-06 19:43:27 +0000

    Finished project

---
+++ innoloft/src/redux/appIdSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const slice = createSlice({
+  name: 'appId',
+  initialState: {
+    id: [],
+  },
+  reducers: {
+    getAppId: (state, action) => {
+      state.id = action.payload;
+    },
+  },
+});
+
+export const { getAppId } = slice.actions;
+
+export const getAppIdData = (state) => state.appId.id;
+
+export default slice.reducer;
